2e1785a772b6bc2d6fd468d670b19cacf9c409f1
王小错 <eliwang@vip.qq.com>
2021-01-12 15:57:09 +0800

    工作年限组件

---
+++ docs/.vuepress/components/WorkingYears.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="WorkingYears">
+    {{ workingYears }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'WorkingYears',
+  data() {
+    return {
+      workingYears: ''
+    }
+  },
+  created() {
+    const currDatetime = +new Date()
+    const graduationDatetime = +new Date('2016/06/30')
+    let workingYears = (currDatetime - graduationDatetime) / 1000 / 60 / 60 / 24 / 365
+    workingYears = workingYears.toFixed(1)
+    this.workingYears = workingYears
+  }
+}
+</script>
