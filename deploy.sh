3d82ff3a1e93f61fffe816aa25a858186a1afecb
王小错 <eliwang@vip.qq.com>
2021-02-06 16:01:25 +0800

    发版 shell

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+npm run docs:build
+cd docs/.vuepress/dist
+git init
+git add .
+time=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "王小错 在 ${time} 更新"
+git remote add origin git@github.com:wangxiaocuo/wangxiaocuo.github.io.git
+git push -u --force origin master
