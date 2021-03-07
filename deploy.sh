b3bb650d9149fe33da23ceaa5590fb07d4a860a7
王小错 <eliwang@vip.qq.com>
2021-03-07 16:00:40 +0800

    deploy.sh matser -> main

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ git add .
 time=$(date "+%Y-%m-%d %H:%M:%S")
 git commit -m "王小错 在 ${time} 更新"
 git remote add origin git@github.com:wangxiaocuo/wangxiaocuo.github.io.git
-git push -u --force origin master
+git push -u --force origin main
