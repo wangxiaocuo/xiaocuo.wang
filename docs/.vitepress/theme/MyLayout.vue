26fe8f787c1c5ae524414e0d050851c8cad5f838
wangxiaocuo <xiaocuo.wang@foxmail.com>
2023-10-08 13:36:36 +0800

    feat: 移除冗余代码

--- docs/.vitepress/theme/MyLayout.vue
+++ docs/.vitepress/theme/MyLayout.vue
@@ -1,16 +1,14 @@
 <script setup lang="ts">
-import { useData } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import HomePosts from './HomePosts.vue'
 
 const { Layout } = DefaultTheme
-const { frontmatter } = useData()
 </script>
 
 <template>
   <Layout>
     <template #home-hero-after>
-      <HomePosts v-if="frontmatter.index" />
+      <HomePosts />
     </template>
   </Layout>
 </template>
